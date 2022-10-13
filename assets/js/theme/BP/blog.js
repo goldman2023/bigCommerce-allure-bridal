223568e88fcfc4275ba0d9ce7d2afeaa47dff075
chandraroyalcyber21 <chandrasekhar.n@royalcyber.com>
2022-10-13 16:21:42 +0530

    Blog page

--- assets/js/theme/BP/blog.js
+++ assets/js/theme/BP/blog.js
@@ -1,5 +1,10 @@
 import PageManager from '../page-manager';
-
+import {
+    blockElement3ImagesScreenWidth,
+    blockElementFullscreenImage,
+    blockElementImages2ColumnRight,
+    blockElementImageLeftCopyRight,
+    } from '../BP/universal-blocks';
 export default class Blog extends PageManager {
     constructor(context) {
         super(context);
@@ -7,7 +12,23 @@ export default class Blog extends PageManager {
     
     onReady() {
         let blogJson = this.context.blogJson;
-        console.log('context', JSON.parse(blogJson.replace( /(<([^>]+)>)/ig, '')));
+        let metadata = JSON.parse(blogJson.replace( /(<([^>]+)>)/ig, ''));
+
+        metadata.contentBlocksCollection.items.forEach(element => {
+            if(element.__typename === "BlockElementFullscreenImage"){
+                blockElementFullscreenImage('blockElementFullscreenImage',element);
+            }
+            if(element.__typename === "BlockElement3ImagesScreenWidth"){
+                blockElement3ImagesScreenWidth('blockElement3ImagesScreenWidth',element);
+            }
+            if(element.__typename === "BlockElementImages2ColumnRight"){
+                console.log("hi");
+                blockElementImages2ColumnRight('blockElementImages2ColumnRight',element);
+            }
+            if(element.__typename === "BlockElementImageLeftCopyRight"){
+                blockElementImageLeftCopyRight('blockElementImageLeftCopyRight',element);
+            }
+        });
     }
 
 }
