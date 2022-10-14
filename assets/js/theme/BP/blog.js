2fc986affd78d34f3322b63dec97d03028ec64b6
chandraroyalcyber21 <chandrasekhar.n@royalcyber.com>
2022-10-14 14:24:49 +0530

    Blog post page

--- assets/js/theme/BP/blog.js
+++ assets/js/theme/BP/blog.js
@@ -4,25 +4,30 @@ import {
     blockElementFullscreenImage,
     blockElementImages2ColumnRight,
     blockElementImageLeftCopyRight,
+    blogpostTopBanner,
+    blogpostContentBlock
     } from '../BP/universal-blocks';
 export default class Blog extends PageManager {
     constructor(context) {
         super(context);
     } 
-    
+
     onReady() {
         let blogJson = this.context.blogJson;
         let metadata = JSON.parse(blogJson.replace( /(<([^>]+)>)/ig, ''));
+        console.log(metadata);
+
+        blogpostTopBanner('topBanner',metadata.headlineImage,metadata.headline);
+        blogpostContentBlock('content-section',metadata.bodyCopy);
 
         metadata.contentBlocksCollection.items.forEach(element => {
             if(element.__typename === "BlockElementFullscreenImage"){
-                blockElementFullscreenImage('blockElementFullscreenImage',element);
+                blockElementFullscreenImage('blockElementFullscreenImage',element); 
             }
             if(element.__typename === "BlockElement3ImagesScreenWidth"){
                 blockElement3ImagesScreenWidth('blockElement3ImagesScreenWidth',element);
             }
             if(element.__typename === "BlockElementImages2ColumnRight"){
-                console.log("hi");
                 blockElementImages2ColumnRight('blockElementImages2ColumnRight',element);
             }
             if(element.__typename === "BlockElementImageLeftCopyRight"){
